5bbbb77226df76d5570321301bdb65bd63996aa0
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-01 17:56:37 +0500

    absolute path set

---
+++ src/screens/TextUnify/index.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const TextUnify = () => {
+    return (
+        <div>
+
+            <Navbar />
+            <div>TextUnify</div>
+
+        </div>)
+}
+
+export default TextUnify
