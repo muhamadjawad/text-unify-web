bdae2a1e250b3e214a6bf01543b971b5be84d6a5
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-04 11:55:03 +0500

    text unify screen folder created in screens

---
+++ src/screens/TextUnify/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Editor from '@/components/Editor';
+import TextOutput from '@/components/TextInput';
+import './TextUnify.css';
+
+const TextUnify: React.FC = () => {
+  const [input, setInput] = React.useState<string>('');
+
+  return (
+    <div className="textunify-container">
+      <div className="textunify-input-wrapper">
+        <Editor value={input} onChange={setInput} />
+      </div>
+      <div className="textunify-output-wrapper">
+        <TextOutput value={input} />
+      </div>
+    </div>
+  );
+};
+
+export default TextUnify; 
