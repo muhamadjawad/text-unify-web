133544a623346209a794d298faf3748f9a33af48
muhamadjawad <jawadmansoor1998@gmail.com>
2025-07-05 19:17:42 +0500

    dev

---
+++ src/utils/info.ts
@@ -0,0 +1,9 @@
+const INFO =
+{
+    MY_LINKEDIN: 'https://www.linkedin.com/in/muhammad-jawad-react-native-reactjs-nodejs-nextjs-expressjs/',
+    MY_GITHUB: 'https://github.com/muhamadjawad',
+    APP_GIT_REPO: 'https://github.com/muhamadjawad/text-unify-web'
+}
+
+
+export { INFO };
